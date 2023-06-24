e305b761cf1b9288904ac5e28822f9fac0a8dc9c
Pedro Vinicius <pedro.santos@metasix.com.br>
2023-06-24 10:59:57 -0300

    Iniciando o projeto com Spring Boot

---
+++ src/main/java/br/com/grupo27/techchallange01/Techchallange01Application.java
@@ -0,0 +1,13 @@
+package br.com.grupo27.techchallange01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Techchallange01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Techchallange01Application.class, args);
+	}
+
+}

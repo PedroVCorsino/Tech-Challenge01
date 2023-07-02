85cb0715d668f69ac130b6fe096ce76e4897b944
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2023-07-02 18:23:44 -0300

    Refatorando logica para busca de pordutos

--- src/main/java/br/com/grupo27/techchallange01/core/domain/ports/service/ProdutoService.java
+++ src/main/java/br/com/grupo27/techchallange01/core/domain/ports/service/ProdutoService.java
@@ -5,15 +5,8 @@ import java.util.List;
 import br.com.grupo27.techchallange01.core.application.dto.ProdutoDTO;
 
 public interface ProdutoService {
-
-    ProdutoDTO getProdutoById(Long id);
-
     List<ProdutoDTO> getAllProdutos();
 
-    ProdutoDTO createProduto(ProdutoDTO produtoDTO);
-
-    ProdutoDTO updateProduto(Long id, ProdutoDTO produtoDTO);
-
-    boolean deleteProduto(Long id);
+    List<ProdutoDTO> getProdutosByTipo(String tipo);
     
 }

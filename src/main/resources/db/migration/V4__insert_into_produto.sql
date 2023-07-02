427524e109f33f140037b27004ef5c7f7db2ddad
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2023-07-02 16:24:12 -0300

    - Add flyway migration para popular banco de dados - Substituindo h2 por postgres

---
+++ src/main/resources/db/migration/V4__insert_into_produto.sql
@@ -0,0 +1,27 @@
+-- Lanches
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Hambúrguer', 'Delicioso hambúrguer artesanal', 10.99, 'LANCHE');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Cheeseburguer', 'Hambúrguer com queijo derretido', 12.99, 'LANCHE');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('X-Bacon', 'Hambúrguer com bacon crocante', 14.99, 'LANCHE');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Vegetariano', 'Lanche saudável com ingredientes naturais', 9.99, 'LANCHE');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Frango', 'Lanche com suculento frango grelhado', 11.99, 'LANCHE');
+
+-- Acompanhamentos
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Batata Frita', 'Porção de batatas fritas crocantes', 5.99, 'ACOMPANHAMENTO');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Onion Rings', 'Deliciosos anéis de cebola empanados', 6.99, 'ACOMPANHAMENTO');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Nuggets', 'Pedacinhos de frango empanados', 7.99, 'ACOMPANHAMENTO');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Salada Caesar', 'Salada fresca com molho Caesar', 8.99, 'ACOMPANHAMENTO');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Mandioca Frita', 'Porção de mandioca frita crocante', 4.99, 'ACOMPANHAMENTO');
+
+-- Bebidas
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Refrigerante', 'Bebida gaseificada sabor cola', 4.99, 'BEBIDA');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Suco Natural', 'Suco de frutas frescas', 5.99, 'BEBIDA');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Água Mineral', 'Água sem gás', 2.99, 'BEBIDA');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Chá Gelado', 'Chá gelado sabor limão', 3.99, 'BEBIDA');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Café', 'Café fresco e quente', 2.99, 'BEBIDA');
+
+-- Sobremesas
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Bolo de Chocolate', 'Delicioso bolo de chocolate', 7.99, 'SOBREMESA');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Pudim de Leite', 'Clássico pudim de leite condensado', 6.99, 'SOBREMESA');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Sorvete de Morango', 'Sorvete cremoso sabor morango', 5.99, 'SOBREMESA');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Mousse de Maracujá', 'Mousse refrescante de maracujá', 4.99, 'SOBREMESA');
+INSERT INTO PRODUTO (nome, descricao, preco, TIPO_PRODUTO) VALUES ('Torta de Limão', 'Torta doce com recheio de limão', 6.99, 'SOBREMESA');

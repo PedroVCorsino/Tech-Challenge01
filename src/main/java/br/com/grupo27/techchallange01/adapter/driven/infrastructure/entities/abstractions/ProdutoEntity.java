4b2a4e353dbf0d122ac3841e8d86638896022a34
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2023-07-02 16:28:37 -0300

    Corrigindo a geração dos IDs

--- src/main/java/br/com/grupo27/techchallange01/adapter/driven/infrastructure/entities/abstractions/ProdutoEntity.java
+++ src/main/java/br/com/grupo27/techchallange01/adapter/driven/infrastructure/entities/abstractions/ProdutoEntity.java
@@ -19,7 +19,7 @@ import java.math.BigDecimal;
 public abstract class ProdutoEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 

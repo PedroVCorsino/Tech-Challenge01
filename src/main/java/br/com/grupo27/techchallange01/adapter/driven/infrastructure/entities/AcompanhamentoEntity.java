cd3a82a311083b486517f82f5f45082f8aad5930
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2023-07-03 22:17:01 -0300

    Refatorando entities dos produtos para que as mesmas não sejam mais extensões de ProdutoEntity

--- src/main/java/br/com/grupo27/techchallange01/adapter/driven/infrastructure/entities/AcompanhamentoEntity.java
+++ src/main/java/br/com/grupo27/techchallange01/adapter/driven/infrastructure/entities/AcompanhamentoEntity.java
@@ -2,14 +2,63 @@ package br.com.grupo27.techchallange01.adapter.driven.infrastructure.entities;
 
 import java.math.BigDecimal;
 
-import br.com.grupo27.techchallange01.adapter.driven.infrastructure.entities.abstractions.ProdutoEntity;
 import br.com.grupo27.techchallange01.core.domain.model.Acompanhamento;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "ACOMPANHAMENTO")
-public class AcompanhamentoEntity extends ProdutoEntity {
+public class AcompanhamentoEntity  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "preco", nullable = false)
+    private BigDecimal preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
 
     protected AcompanhamentoEntity() {}
 
@@ -19,7 +68,7 @@ public class AcompanhamentoEntity extends ProdutoEntity {
         this.setDescricao(descricao);
         this.setPreco(preco);
     }
-
+    
     public Acompanhamento toAcompanhamento() {
         return new Acompanhamento(this.getId(), this.getNome(), this.getDescricao(), this.getPreco());
     }

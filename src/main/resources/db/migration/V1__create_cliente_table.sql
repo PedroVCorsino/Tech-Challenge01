3cbf0b29416cd36287ec2d45ee3641aa0792e5f5
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2023-07-08 18:17:26 -0300

    - removendo comentarios desnecessarios - Criando migration para popular banco com pedidos e combos

--- src/main/resources/db/migration/V1__create_cliente_table.sql
+++ src/main/resources/db/migration/V1__create_cliente_table.sql
@@ -4,15 +4,3 @@ CREATE TABLE IF NOT EXISTS cliente (
   nome VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL
 );
-
-
--- INSERT INTO CLIENTE (cpf, nome, email) VALUES ('12345678901', 'João da Silva', 'joao@example.com');
--- INSERT INTO CLIENTE (cpf, nome, email) VALUES ('98765432109', 'Maria Souza', 'maria@example.com');
--- INSERT INTO CLIENTE (cpf, nome, email) VALUES ('45678912345', 'Pedro Oliveira', 'pedro@example.com');
--- INSERT INTO CLIENTE (cpf, nome, email) VALUES ('78901234567', 'Ana Santos', 'ana@example.com');
--- INSERT INTO CLIENTE (cpf, nome, email) VALUES ('23456789012', 'Lucas Pereira', 'lucas@example.com');
--- INSERT INTO CLIENTE (cpf, nome, email) VALUES ('56789012345', 'Mariana Fernandes', 'mariana@example.com');
--- INSERT INTO CLIENTE (cpf, nome, email) VALUES ('90123456789', 'Rafaela Costa', 'rafaela@example.com');
--- INSERT INTO CLIENTE (cpf, nome, email) VALUES ('34567890123', 'Carlos Rodrigues', 'carlos@example.com');
--- INSERT INTO CLIENTE (cpf, nome, email) VALUES ('67890123456', 'Juliana Almeida', 'juliana@example.com');
--- INSERT INTO CLIENTE (cpf, nome, email) VALUES ('01234567890', 'Gustavo Santos', 'gustavo@example.com');
--- src/main/resources/db/migration/V3__create_produto_table.sql
+++ src/main/resources/db/migration/V3__create_produto_table.sql
@@ -1,11 +1,3 @@
--- CREATE TABLE PRODUTO (
---   id SERIAL PRIMARY KEY,
---   nome VARCHAR(255),
---   descricao VARCHAR(255),
---   preco DECIMAL(10,2) NOT NULL,
---   TIPO_PRODUTO VARCHAR(20)
--- );
-
 CREATE TABLE LANCHE (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255),
@@ -32,30 +24,4 @@ CREATE TABLE SOBREMESA (
     nome VARCHAR(255),
     descricao VARCHAR(255),
     preco DECIMAL(10, 2) NOT NULL
-);
-
-
-CREATE TABLE PEDIDO (
-  id SERIAL PRIMARY KEY,
-  id_cliente BIGINT NOT NULL,
-  valor_total DECIMAL(10, 2),
-  status VARCHAR(20) NOT NULL,
-  pago BOOLEAN NOT NULL
-);
-
-CREATE TABLE COMBO (
-  id SERIAL PRIMARY KEY,
-  lanche_id BIGINT,
-  acompanhamento_id BIGINT,
-  bebida_id BIGINT,
-  sobremesa_id BIGINT,
-  quantidade INT NOT NULL,
-  valor_unitario DECIMAL(10, 2) NOT NULL,
-  valor_total DECIMAL(10, 2) NOT NULL,
-  pedido_id BIGINT,
-  FOREIGN KEY(lanche_id) REFERENCES LANCHE(id),
-  FOREIGN KEY(acompanhamento_id) REFERENCES ACOMPANHAMENTO(id),
-  FOREIGN KEY(bebida_id) REFERENCES BEBIDA(id),
-  FOREIGN KEY(sobremesa_id) REFERENCES SOBREMESA(id),
-  FOREIGN KEY(pedido_id) REFERENCES PEDIDO(id)
 );

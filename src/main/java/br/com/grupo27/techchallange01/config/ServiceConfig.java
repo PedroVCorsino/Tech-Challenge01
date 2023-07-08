86893da38073a95375781d01fade4ad1bffc9af9
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2023-07-08 18:14:09 -0300

    Add novas dependencias para pedido service e pagamentoClient

--- src/main/java/br/com/grupo27/techchallange01/config/ServiceConfig.java
+++ src/main/java/br/com/grupo27/techchallange01/config/ServiceConfig.java
@@ -1,25 +1,27 @@
 package br.com.grupo27.techchallange01.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import br.com.grupo27.techchallange01.adapter.driven.infrastructure.repositories.PedidoRepositoryAdapter;
 import br.com.grupo27.techchallange01.adapter.driven.infrastructure.repositories.JPA.PedidoJPA;
 import br.com.grupo27.techchallange01.config.mappers.PedidoMapper;
+import br.com.grupo27.techchallange01.core.application.clients.PagamentosClientImpl;
 import br.com.grupo27.techchallange01.core.application.services.PedidoServiceImpl;
+import br.com.grupo27.techchallange01.core.domain.ports.client.PagamentosClient;
 
 @Configuration
 public class ServiceConfig {
 
-    // @Bean
-    // public LancheServiceImpl lancheService(LancheJPA lancheJPA, LancheMapper lancheMapper) {
-    //     return new LancheServiceImpl(new LancheRepositoryAdapter(lancheJPA, lancheMapper), lancheMapper);
-    // }
+    @Bean
+    public PedidoServiceImpl pedidoService(PedidoJPA pedidoJPA, PedidoMapper pedidoMapper, PagamentosClient pagamentosClient) {
+        return new PedidoServiceImpl(new PedidoRepositoryAdapter(pedidoJPA, pedidoMapper), pedidoMapper, pagamentosClient);
+    }
 
     @Bean
-    public PedidoServiceImpl pedidoService(PedidoJPA pedidoJPA, PedidoMapper pedidoMapper) {
-        return new PedidoServiceImpl(new PedidoRepositoryAdapter(pedidoJPA, pedidoMapper), pedidoMapper);
+    public PagamentosClient pagamentosClient(WebClient.Builder webClientBuilder) {
+        return new PagamentosClientImpl(webClientBuilder);
     }
 
 }
